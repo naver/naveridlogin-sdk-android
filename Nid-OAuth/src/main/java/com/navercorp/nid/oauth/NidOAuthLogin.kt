8ce37c920b8fdeea67799721835c5e7a868b2511
David Khol <david.khol@bucketplace.net>
2026-01-14 15:10:43 +0900

    Do not misuse CoroutineScope in NidOAuthLogin

--- Nid-OAuth/src/main/java/com/navercorp/nid/oauth/NidOAuthLogin.kt
+++ Nid-OAuth/src/main/java/com/navercorp/nid/oauth/NidOAuthLogin.kt
@@ -8,9 +8,8 @@ import com.navercorp.nid.oauth.util.NidOAuthCallback
 import com.navercorp.nid.profile.domain.vo.NidProfile
 import com.navercorp.nid.profile.domain.vo.NidProfileMap
 import com.navercorp.nid.profile.util.NidProfileCallback
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -19,23 +18,22 @@ class NidOAuthLogin {
         Login(NidServiceLocator.provideOAuthRepository())
     }
 
-    private val coroutineExceptionHandler = CoroutineExceptionHandler { _, throwable ->
-        NidLog.e(TAG, "CoroutineExceptionHandler $throwable")
-    }
-    private val ioScope = CoroutineScope(Dispatchers.IO + coroutineExceptionHandler)
-
     @Deprecated(
         message = "This method will be removed from v6.1.0. Use NidOAuth.requestLogin(cotext, callback) instead.",
         replaceWith = ReplaceWith("NidOAuth.requestLogin(context, callback)"),
     )
-    fun callRefreshAccessTokenApi(callback: NidOAuthCallback) = ioScope.launch {
-        val oauthToken = login.refreshToken()
-        withContext(Dispatchers.Main) {
-            if (oauthToken.isOAuthSuccess) {
-                callback.onSuccess()
-            } else {
-                callback.onFailure(oauthToken.error.code, oauthToken.errorDescription)
+    fun callRefreshAccessTokenApi(callback: NidOAuthCallback) = GlobalScope.launch(Dispatchers.IO) {
+        try {
+            val oauthToken = login.refreshToken()
+            withContext(Dispatchers.Main) {
+                if (oauthToken.isOAuthSuccess) {
+                    callback.onSuccess()
+                } else {
+                    callback.onFailure(oauthToken.error.code, oauthToken.errorDescription)
+                }
             }
+        } catch (throwable: Throwable) {
+            NidLog.e(TAG, "CoroutineExceptionHandler $throwable")
         }
     }
 
