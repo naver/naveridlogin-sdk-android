f0672ad10a9724c444821c18dbae948d4b2465a5
yu-ri-mun <yu_ri.mun@navercorp.com>
2026-02-20 17:23:45 +0900

    [feat] 누락된 buildSrc 내 변경사항 업데이트

--- buildSrc/src/main/java/Configurations.kt
+++ buildSrc/src/main/java/Configurations.kt
@@ -1,19 +1,10 @@
 object Configurations {
-    /* Kotlin */
-    const val kotlinVersion = "2.1.0"
-
     /* Module */
     const val compileSdkVersion = 35
     const val targetSdkVersion = 35
     const val minSdkVersion = 21
 
     /* Version */
-    const val moduleVersionName = "5.11.0"
-    const val moduleVersionCode = 5_11_00
-
-    /* Project ClassPath */
-    object Plugins {
-        const val android = "com.android.tools.build:gradle:8.6.1"
-        const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion"
-    }
+    const val moduleVersionName = "5.11.2"
+    const val moduleVersionCode = 5_11_02
 }
